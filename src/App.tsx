00b2150204838c2d20619056a9ca8be0bb990097
Kamil Musiał <kmusial40@gmail.com>
2020-02-16 16:17:27 +0100

    scss setup
    add simple functions mixins and variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './scss/global/main.scss';
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>aa</h1>
+        </div>
+    );
 }
 
 export default App;
