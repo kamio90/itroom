0c193fca4b498b94f9bf0838327f9c32f1f92bf6
Kamil Musiał <kmusial40@gmail.com>
2020-02-17 16:55:22 +0100

    Nav component setup
    Settting nav component nav styles and home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './scss/global/main.scss';
+import Home from './pages/home';
 
 function App() {
     return (
         <div className="App">
-            <h1>aa</h1>
+            <Home />
         </div>
     );
 }
---
+++ src/components/nav.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../scss/nav/nav.scss';
+
+export default class Nav extends React.Component {
+    render() {
+        return (
+            <nav className="nav" role="navigation">
+                <div className="nav__logo">a</div>
+            </nav>
+        );
+    }
+}
