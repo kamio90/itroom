5f54ab256ed15fa89f3331c3377f4dc11a8862a9
Kamil Musiał <kmusial40@gmail.com>
2020-02-18 13:35:50 +0100

    style for nav component
    create markup for nav comopennt and css for it

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -6,8 +6,29 @@ export default class Nav extends React.Component {
     render() {
         return (
             <nav className="nav" role="navigation">
-                <div className="nav__logo">a</div>
-                <ul className="nav__list">a</ul>
+                <div className="nav__logo"></div>
+                <ul className="nav__list">
+                    <li className="nav__list__item">
+                        <a href="foo" className="nav__list__item__link">
+                            O nas
+                        </a>
+                    </li>
+                    <li className="nav__list__item">
+                        <a href="foo" className="nav__list__item__link">
+                            Realizacje
+                        </a>
+                    </li>
+                    <li className="nav__list__item">
+                        <a href="foo" className="nav__list__item__link">
+                            Usługi
+                        </a>
+                    </li>
+                    <li className="nav__list__item">
+                        <a href="foo" className="nav__list__item__link">
+                            Kontakt
+                        </a>
+                    </li>
+                </ul>
                 <Button className="nav__btn" content="Zamów połączenie" />
             </nav>
         );
