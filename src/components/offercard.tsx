3f0b38a03980477957933ecc323dcf53f87e3600
Kamil Musiał <kmusial40@gmail.com>
2020-02-19 15:39:48 +0100

    create simple scss for  offercard
    some style for info card

--- src/components/offercard.tsx
+++ src/components/offercard.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import '../scss/components/offercard.scss';
 
 interface Props {
     title?: string;
     description?: string;
+    src?: string;
+    alt?: string;
 }
 
 export default class OfferCard extends React.Component<Props, {}> {
     render() {
-        const { title, description } = this.props;
+        const { title, description, src } = this.props;
         return (
-            <div className="card">
-                <h3>{title}</h3>
-                <h4>{description}</h4>
+            <div className="card" style={{ backgroundImage: `url(${src})` }}>
+                <h3 className="card__title">{title}</h3>
+                <h4 className="card__description">{description}</h4>
             </div>
         );
     }
--- src/pages/parts/offer.tsx
+++ src/pages/parts/offer.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import OfferCard from '../../components/offercard';
+import '../../scss/pages/home/offer.scss';
+import Web from '../../assets/desk-laptop-macbook-pro-working-69432.jpg';
 
 interface State {
     [index: number]: {
         title?: string;
         description?: string;
+        src?: string;
+        alt?: string;
     };
 }
 
@@ -12,21 +16,25 @@ export default class Offer extends React.Component<{}, State> {
     state = [
         {
             title: 'aa',
-            description: 'aa'
+            description: 'aa',
+            src: Web,
+            alt: 'www.itroom.pl - Zdjęcie wyróżniające karte strony internetowe'
         }
     ];
 
     render() {
         return (
-            <>
+            <section className="offer">
                 {this.state.map((value, key) => (
                     <OfferCard
                         key={key}
                         title={value.title}
                         description={value.description}
+                        src={value.src}
+                        alt={value.src}
                     />
                 ))}
-            </>
+            </section>
         );
     }
 }
