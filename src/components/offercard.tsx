bb5815b81f1fcb30425f18981203d092077a676e
Kamil Musiał <kmusial40@gmail.com>
2020-02-20 17:23:17 +0100

    finish cardinfo style
    add some img and create styles for cardinfo

--- src/components/offercard.tsx
+++ src/components/offercard.tsx
@@ -6,15 +6,22 @@ interface Props {
     description?: string;
     src?: string;
     alt?: string;
+    className?: string;
 }
 
 export default class OfferCard extends React.Component<Props, {}> {
     render() {
-        const { title, description, src } = this.props;
+        const { title, description, src, className } = this.props;
         return (
-            <div className="card" style={{ backgroundImage: `url(${src})` }}>
-                <h3 className="card__title">{title}</h3>
-                <h4 className="card__description">{description}</h4>
+            <div className="card">
+                <div
+                    className={`card__background card__background${className}`}
+                    style={{ backgroundImage: `url(${src})` }}
+                ></div>
+                <div className="card__inner">
+                    <h3 className="card__inner__title">{title}</h3>
+                    <h4 className="card__inner__description">{description}</h4>
+                </div>
             </div>
         );
     }
--- src/pages/parts/offer.tsx
+++ src/pages/parts/offer.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import OfferCard from '../../components/offercard';
 import '../../scss/pages/home/offer.scss';
 import Web from '../../assets/desk-laptop-macbook-pro-working-69432.jpg';
+import Mobile from '../../assets/black-iphone-7-on-brown-table-699122.jpg';
+import Desctop from '../../assets/apple-devices-books-business-coffee-572056.jpg';
+import Server from '../../assets/woman-holding-laptop-beside-glass-wall-1181316.jpg';
+import Support from '../../assets/woman-wearing-earpiece-using-white-laptop-computer-210647.jpg';
+import Learning from '../../assets/working-woman-technology-computer-7374.jpg';
 
 interface State {
     [index: number]: {
@@ -9,16 +14,58 @@ interface State {
         description?: string;
         src?: string;
         alt?: string;
+        className?: string;
     };
 }
 
 export default class Offer extends React.Component<{}, State> {
     state = [
         {
-            title: 'aa',
-            description: 'aa',
+            title: 'Strony Internetowe',
+            description:
+                'Oferujemy stworzenie idealnej storny internetowej skrojonej na twoje potrzeby',
             src: Web,
-            alt: 'www.itroom.pl - Zdjęcie wyróżniające karte strony internetowe'
+            alt:
+                'www.itroom.pl - Zdjęcie wyróżniające karte strony internetowe',
+            className: '-lightblue'
+        },
+        {
+            title: 'Aplikacje Mobilne',
+            description:
+                'Oferujemy stworzenie autorskiej aplikacji działającej pod wszytkimi systemami',
+            src: Mobile,
+            alt: 'www.itroom.pl - Zdjęcie wyróżniające karte aplikacje mobilne',
+            className: '-superlightblue'
+        },
+        {
+            title: 'Aplikacje Komputerowe',
+            description: 'Oferujemy stworzenie programu komputerowego',
+            src: Desctop,
+            alt:
+                'www.itroom.pl - Zdjęcie wyróżniające karte aplikacje komputerowe',
+            className: '-darkpurple'
+        },
+        {
+            title: 'Rozwiązania serwerowe',
+            description: 'Oferujemy optymalizacje oraz zabezpieczenie serwerów',
+            src: Server,
+            alt:
+                'www.itroom.pl - Zdjęcie wyróżniające karte rozwiązań serwerowych',
+            className: '-darkyellow'
+        },
+        {
+            title: 'Wsparcie IT',
+            description: 'Oferujemy wsparcie it dla firm',
+            src: Support,
+            alt: 'www.itroom.pl - Zdjęcie wyróżniające karte wsparcie it',
+            className: '-darkred'
+        },
+        {
+            title: 'Szkolenia IT',
+            description: 'Oferujemy szkolenia IT',
+            src: Learning,
+            alt: 'www.itroom.pl - Zdjęcie wyróżniające karte szkolenia it',
+            className: '-darkblue'
         }
     ];
 
@@ -32,6 +79,7 @@ export default class Offer extends React.Component<{}, State> {
                         description={value.description}
                         src={value.src}
                         alt={value.src}
+                        className={value.className}
                     />
                 ))}
             </section>
